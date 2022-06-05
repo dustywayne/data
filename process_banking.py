6203dc4b933913adcaf3a3be8780000fb6be5797
Dustin Adams <dustinadams@MacBook-Air.attlocal.net>
2022-06-04 20:57:50 -0500

    Updating process_banking.py

--- process_banking.py
+++ process_banking.py
@@ -3,6 +3,12 @@ import numpy as np
 from IPython import embed as shell
 
 # read independent variables
-df = pd.read_csv('banking/independent_variable.csv')
-shell()
+df = pd.read_csv('banking/independent_variables.csv')
 
+age = np.array(df['age'], dtype=str)
+job = np.array(df['job'], dtype=str)
+marital = np.array(df['marital'], dtype=str)
+education = np.array(df['education'], dtype=str)
+
+loan = np.array(pd.read_csv('banking/dependent_variable.csv')['loan'], dtype=str)
+loan = [a == 'yes' for a in loan]
